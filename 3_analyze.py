52513fea19210bf669311e3cf2cf4274b9220d01
Jon Gauthier <jon@gauthiers.net>
2020-05-22 11:20:20 -0400

    stage missing 3_analyze changes

--- 3_analyze.py
+++ 3_analyze.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 import sys
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
 
@@ -11,7 +12,7 @@ def main(args):
 
   df = pd.read_table(args.file, header=None)
 
-  fig, axs = plt.subplots(2)
+  fig, axs = plt.subplots(3)
   fig.suptitle(args.file)
 
   adj_type_order = df[2].value_counts().index
@@ -22,13 +23,13 @@ def main(args):
   # Plot type/token ratios.
   token_frequencies = df[2].value_counts()
   type_frequencies = df.groupby(2).agg({0: 'nunique'}).reindex(token_frequencies.index)
-  type_token_ratios = type_frequencies.div(token_frequencies, axis=0)
+  type_token_ratios = type_frequencies.copy().div(token_frequencies.apply(np.log), axis=0)
   type_token_ratios = type_token_ratios.rename({0: "type_token_ratio"}, axis=1).reset_index()
 
   sns.barplot(x="index", y="type_token_ratio", data=type_token_ratios, ax=axs[1])
 
   # Type frequencies
-  # sns.barplot(x="index", y=0, data=type_frequencies.reset_index(), ax=axs[2])
+  sns.barplot(x="index", y=0, data=type_frequencies.reset_index(), ax=axs[2])
 
   axs[0].set_title("token frequency")
   axs[0].set_xlabel("adj type")
@@ -45,10 +46,19 @@ def main(args):
   else:
     plt.show()
 
-  # Print corpus statistics.
   ret = type_token_ratios.copy()
+
+  # Prep corpus statistics.
   ret["corpus"] = args.file
   ret = ret.assign(token_frequency=token_frequencies.values)
+  # ret["token_rel_frequency"] = ret["token_frequency"] / ret["token_frequency"].sum()
+
+  # DEV: retain only shape/color.
+  ret = ret[ret["index"].isin(["shape", "color"])]
+  ret["token_rel_frequency"] = ret["token_frequency"] / ret["token_frequency"].sum()
+  n = ret["token_frequency"].sum()
+  ret = ret.assign(stderr=np.sqrt(ret["token_rel_frequency"] * (1 - ret["token_rel_frequency"])) / np.sqrt(n))
+
   ret.to_csv(sys.stdout, sep="\t")
 
 
